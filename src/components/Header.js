331017dd076634fb8e5a503b2396e54b4989a809
kishankd309 <kishankubavat309@gmail.com>
2024-10-27 00:13:31 +0530

    yes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+//import components
+import Nav from "../components/Nav";
+import Socials from "../components/Socials";
+// import logo
+import LogoWhite from "../assets/img/header/logo-white.png";
+
+//import motion
+import { motion } from "framer-motion";
+
+//import variants
+import { fadeIn, staggerContainer } from "../variants";
+
+// header variants
+const headerVariants = {
+  hidden: {
+    padding: "84px 0 84px 0",
+    background: "none",
+  },
+  show: {
+    padding: "14px 0 14px 0",
+    background:"rgba(0,0,0,0.92)"
+  },
+};
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className="bg-pink-200">header</header>;
 };
 
 export default Header;
